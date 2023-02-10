14d5b114e75d30248e23e166cd3419f2cb58af50
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 20:06:39 -0300

    update

---
+++ Csharp-orientação-a-objetos/0-exercicios/exemplo-exercicio/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+class Programa
+{
+    static void Main(string[] args)
+    {
+        double salario = 3300.0;
+
+        if (salario < 2600.0)
+        {
+            Console.WriteLine("A sua aliquota é de 15%");
+            Console.WriteLine("Você pode deduzir até R$ 350");
+        }
+        if (salario < 3750.0)
+        {
+            Console.WriteLine("A sua aliquota é de 22,5%");
+            Console.WriteLine("Você pode deduzir até R$ 636");
+        }
+    }
+}
