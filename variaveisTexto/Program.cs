b70914a7cef867c1831d8a59aa5c45c8ed2f2e64
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 10:21:51 -0300

    update add projeto 5

---
+++ variaveisTexto/Program.cs
@@ -0,0 +1,12 @@
+﻿using Internal;
+using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 5 - Caracteres e Textos");
+
+        Console.WriteLine("Tecle ENTER para fechar ...");
+        Console.ReadLine();
+    }    
+}
