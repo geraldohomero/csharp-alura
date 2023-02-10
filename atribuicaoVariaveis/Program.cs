c9c26a951ebcfebb53861068562d5fade8453692
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 10:46:02 -0300

    update projeto 6

---
+++ atribuicaoVariaveis/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 6 - Atribuições de variaveis");
+
+        int idade = 30;
+        int idadeAna = idade;
+        Console.WriteLine(idadeAna);
+
+        Console.WriteLine("Tecle ENTER para fechar ...");
+        Console.ReadLine();
+    }
+}
+
