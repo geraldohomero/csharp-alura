eb5e757c43361eb9af20a86666d65c4515941ec7
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 18:23:46 -0300

    update-test-githubdesktop

--- condicionais/Program.cs
+++ condicionais/Program.cs
@@ -6,6 +6,7 @@ class Programa
         Console.WriteLine("Projeto 7 - Condicionais");
 
         int idadeJoao = 16;
+        int idade = 18;
 
         Console.WriteLine("Tecle enter para fechar ...");
         Console.ReadLine();
