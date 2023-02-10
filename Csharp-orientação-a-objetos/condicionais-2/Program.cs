f8767c0de199db307bc55d55a1028e893f05ce4c
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 19:50:49 -0300

    update

--- Csharp-orientação-a-objetos/condicionais-2/Program.cs
+++ Csharp-orientação-a-objetos/condicionais-2/Program.cs
@@ -3,7 +3,28 @@ class Programa
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Olá, mundo");
+        Console.WriteLine("Projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+        int pessoasComJoao = 2;
+
+        bool acompanhado = pessoasComJoao > 0;
+        bool grupo = true;
+
+        Console.WriteLine("João tem " + idadeJoao);
+        Console.WriteLine("Ele está com " + pessoasComJoao + " pessoas");
+
+        if (idadeJoao >= 18 || acompanhado)
+        {
+            Console.WriteLine("Pode entrar!");
+        }
+        else
+        {
+
+            Console.WriteLine("Não pode entrar!");
+           
+        }
+
         Console.WriteLine("Tecle enter para fechar ...");
         Console.ReadLine();
     }
