264d141c4cdbe012f76d0670b2ea553026345093
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 18:47:27 -0300

    add-folder

---
+++ Csharp-orientação-a-objetos/variaveis/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 2 - Criando Variáveis");
+
+        int idade;
+
+        idade = 27;
+
+        Console.WriteLine("Minha idade é " + idade);
+
+        idade = 27 - 5;
+
+        Console.WriteLine(idade);
+
+        idade = 5 * 2 - 6;
+
+        Console.WriteLine(idade);
+
+        idade = (5 - 2) * 3;
+
+        Console.Write(idade);
+        
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
---
+++ Csharp-orientação-a-objetos/variaveisPontoFlutuante/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 3 - Variáveis Ponto Flutuante");
+
+        double salario;
+        salario = 3000.10;
+
+        double idade;
+
+        idade = 7.0 / 5;
+
+        Console.WriteLine(idade);
+
+        Console.WriteLine(salario);
+
+        Console.WriteLine("Tecle enter para fechar...");
+        Console.ReadLine();
+    }
+}
