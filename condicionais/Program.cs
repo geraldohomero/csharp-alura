b571d5debb91c09c4849e47635d7bc6b03dee7cc
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 18:33:44 -0300

    add-update

--- condicionais/Program.cs
+++ condicionais/Program.cs
@@ -6,14 +6,26 @@ class Programa
         Console.WriteLine("Projeto 7 - Condicionais");
 
         int idadeJoao = 17;
+        int pessoasComJoao = 2;
         
+        Console.WriteLine("João tem " + idadeJoao);
+        Console.WriteLine("Ele está com " + pessoasComJoao + " pessoas");
+
         if(idadeJoao >= 18)
         {
             Console.WriteLine("Pode entrar!");
         }
         else
         {
-            Console.WriteLine("Não pode entrar!");
+            if(pessoasComJoao > 0)
+            {
+                Console.WriteLine("Ele está acompanhado. Pode entrar!");
+            }
+            else
+            {
+                Console.WriteLine("Não pode entrar!");
+            }
+        
         }
 
         Console.WriteLine("Tecle enter para fechar ...");
