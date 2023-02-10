18392001fbf78bc6c1454aeaebbff37b5d682cd7
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 10:05:47 -0300

    update

---
+++ conversoes/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Double salario;
+        salario = 3000.15;
+
+        int salarioInteiro;
+        salarioInteiro = (int)salario; //int só trabalha com até 32bites
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
