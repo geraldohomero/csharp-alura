5d3679caa847b2b3d516c0387eba9342851a29d5
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 10:27:51 -0300

    update

--- variaveisTexto/Program.cs
+++ variaveisTexto/Program.cs
@@ -1,10 +1,12 @@
-﻿using Internal;
-using System;
-class Programa
+﻿using System;
+class Program
 {
     static void Main(string[] args)
     {
         Console.WriteLine("Projeto 5 - Caracteres e Textos");
+
+        char letra = 'a';
+        Console.WriteLine(letra);
 
         Console.WriteLine("Tecle ENTER para fechar ...");
         Console.ReadLine();
