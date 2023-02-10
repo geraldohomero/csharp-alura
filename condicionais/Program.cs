a4347d7f730bc455fe30bd12a2b9169b68528719
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 18:22:18 -0300

    update

---
+++ condicionais/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+class Programa
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Projeto 7 - Condicionais");
+
+        int idadeJoao = 16;
+
+        Console.WriteLine("Tecle enter para fechar ...");
+        Console.ReadLine();
+    }
+}
