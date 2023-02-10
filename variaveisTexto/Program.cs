23e83daeb569d993e81f08b483bac3e0510f5dde
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 10:31:35 -0300

    update

--- variaveisTexto/Program.cs
+++ variaveisTexto/Program.cs
@@ -8,6 +8,13 @@ class Program
         char letra = 'a';
         Console.WriteLine(letra);
 
+        letra = (char)65; //ASCII Table
+        Console.WriteLine(letra);
+
+        letra = (char)(65 + 1);
+        Console.WriteLine(letra);
+
+
         Console.WriteLine("Tecle ENTER para fechar ...");
         Console.ReadLine();
     }    
