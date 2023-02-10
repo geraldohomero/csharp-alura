0ad0f0cacb2866172c10588da9b0db14bae9ee87
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-10 10:12:57 -0300

    update

--- conversoes/Program.cs
+++ conversoes/Program.cs
@@ -1,13 +1,27 @@
-﻿using System;
+﻿using Internal;
+using System;
 class Programa
 {
     static void Main(string[] args)
     {
-        Double salario;
-        salario = 3000.15;
+        Double salario = 3000.15;
+        Console.WriteLine(salario);
 
         int salarioInteiro;
         salarioInteiro = (int)salario; //int só trabalha com até 32bites
+        Console.WriteLine(salarioInteiro);
+
+        int x = 2000000000000000000;
+        Console.WriteLine(x);
+
+        long y = 200000000000000000;
+        Console.WriteLine(y);
+
+        short z = 12000;
+        Console.WriteLine(z);
+
+        float altura = 1.77f;
+        Console.WriteLine(altura);
 
         Console.WriteLine("Tecle enter para fechar ...");
         Console.ReadLine();
