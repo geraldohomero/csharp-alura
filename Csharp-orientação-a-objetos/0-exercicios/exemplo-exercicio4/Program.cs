583c22b7e593d8e97fc37fb64dc015ddc58ec452
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-12 22:43:03 -0300

    add exercicio 4

---
+++ Csharp-orientação-a-objetos/0-exercicios/exemplo-exercicio4/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(String[] args)
+    {
+        Console.WriteLine("Mostra os números múltiplos de 3 < 100");
+        for (int i = 1; i < 100; i++ )
+        {
+            if (i % 3 == 0)
+            {
+                Console.WriteLine(i);
+            }
+        }
+    }
+}
