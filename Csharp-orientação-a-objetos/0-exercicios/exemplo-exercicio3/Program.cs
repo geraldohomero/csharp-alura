ea97475b124f11cac09d9c3014a732edd809ded2
Geraldo Homero <geraldohomero@protonmail.com>
2023-02-12 22:38:48 -0300

    add exercicio

---
+++ Csharp-orientação-a-objetos/0-exercicios/exemplo-exercicio3/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Programa
+{
+    static void Main(String[] args)
+    {
+        for(int linha = 0; linha < 9; linha++)
+        {
+            for (int coluna = 0; coluna < 9; coluna++)
+            {
+                if (coluna > linha)
+                {
+                    break;
+                }
+                Console.Write(coluna+1);
+            }
+            Console.WriteLine();
+        }
+    }
+}
