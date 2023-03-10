76d4d0986d0dcf091dab6717c738388d7bdf0b23
Geraldo Homero <geraldohomero@protonmail.com>
2023-03-10 11:11:59 -0300

    add update

---
+++ 2-csharp-orientacao-objetos/byteBank/ContaCorrente.cs
@@ -0,0 +1,21 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace byteBank
+{
+    public class ContaCorrente
+    {
+       public int numero_agencia;
+       public string conta;
+       public string titular;
+       public double saldo; 
+
+       public void Depositar(double valor)
+       {
+        this.saldo += valor;
+       }
+    }
+}
--- 2-csharp-orientacao-objetos/byteBank/Program.cs
+++ 2-csharp-orientacao-objetos/byteBank/Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using byteBank;
+
+ContaCorrente contaDoAndre = new ContaCorrente();
+contaDoAndre.titular = "André Silva";
+contaDoAndre.numero_agencia = 15;
+contaDoAndre.conta = "1010-X";
+contaDoAndre.saldo = 100;
+
+Console.WriteLine("Saldo da conta do André = " + contaDoAndre.saldo);
+
+contaDoAndre.Depositar(100);
+
+Console.WriteLine("Saldo da conta do André pós-deposito = " + contaDoAndre.saldo);
